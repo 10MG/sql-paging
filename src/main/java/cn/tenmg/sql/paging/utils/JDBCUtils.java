835ac7dbd017bac39d09680efb630d401603cbfb
zhaoweijun <wjzhao@aliyun.com>
2022-01-04 18:02:44 +0800

    升级dsl到1.2.4，日志依赖改为slf4j-api，具体日志实现由用户选择（比如logback/log4j）

--- src/main/java/cn/tenmg/sql/paging/utils/JDBCUtils.java
+++ src/main/java/cn/tenmg/sql/paging/utils/JDBCUtils.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * JDBC工具类
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
  */
 public abstract class JDBCUtils {
 
-	private static final Logger log = LogManager.getLogger(JDBCUtils.class);
+	private static final Logger log = LoggerFactory.getLogger(JDBCUtils.class);
 
 	private JDBCUtils() {
 	}
